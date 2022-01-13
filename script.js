7670d4fe1e94b7bec273ef076000e144a783e3f6
its-me-Harsh-Anand <harshvivek.787@gmail.com>
2022-01-13 23:26:56 +0530

    defined simple yet good function

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-/*
-Basic idea of ROT-13 is to rotate the word after 13th word of alphabet
-For example :- 
-MATH
-M = 13
-A = 1
-T = 20
-H = 8
+function encryptMe(word){
+    word = word.toUpperCase()
 
-M + 13 i.e, 13 + 13 = 26 = Z
-A + 13 i.e, 1 + 13 = 14 = N
-T + 13 i.e, 20 + 13 = 33 --------> it crosses the limit i.e, 26 hance 33-26 = 7 = G
-H + 13 i.e, 8 + 13 = 21 = U
+    let hashed = ""
 
-Hence MATH is encrypted to ZNGU
-
-Decryption is also same as encryption
-Just subtract 13 where we have added 13 in encryption
-if it goes negative, for example G = 7 and 7-13 = -6.....just do the following 26 - absoluteValueOf(-6) = 26-6 = 20 = T
-
-Hence ZNGU is decrypted back to MATH
-
-*/
-function rot13(c) {
-    return c.replace(/([a-m])|([n-z])/ig, function($0,$1,$2) {
-        return String.fromCharCode($1 ? $1.charCodeAt(0) + 13 : $2 ? $2.charCodeAt(0) - 13 : 0) || $0;
-    });
+    for(let i=0; i<word.length; i++){
+        const wordNumber = word.charCodeAt(i) + 13
+        if(wordNumber <= 90){
+            hashed+= String.fromCharCode(wordNumber)
+        }else{
+            hashed += String.fromCharCode(wordNumber - 26)
+        }
+    }
+    return hashed
 }
-console.log(rot13("ABJURER nowhere"))
+
+function decryptMe(word){
+    word = word.toUpperCase()
+
+    let hashed = ""
+
+    for(let i=0; i<word.length; i++){
+        const wordNumber = word.charCodeAt(i) - 13
+        if(wordNumber >= 65 ){
+            hashed+= String.fromCharCode(wordNumber)
+        }else{
+            hashed += String.fromCharCode(wordNumber + 26)
+        }
+    }
+    return hashed
+}
+
+console.log(encryptMe("MATH"))
+console.log(decryptMe("ZNGU"))
